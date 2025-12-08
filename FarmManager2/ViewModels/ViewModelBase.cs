c01a89bc1706810aee90b85f053235c90d2cb9b2
DonStar67 <duraghell@bk.ru>
2025-12-08 10:10:55 +0300

    Add files via upload
    Начал делать авторизацию

---
+++ FarmManager2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Net.Http.Headers;
+
+namespace FarmManager2.ViewModels
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void PropertyChange([CallerMemberName] string propertyName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected string _title;
+
+        public string Title
+        {
+            get => _title;
+            protected set
+            {
+                if (_title != value)
+                {
+                 _title = value;
+                    PropertyChange();
+                }
+
+            }
+
+        }
+
+    }
+}
